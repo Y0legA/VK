f8626d162b11fa8888d9b576f9af8ddf1e24aad5
Oleg <myaddub@gmail.com>
2022-11-10 11:28:11 +0300

    готовое 8

--- VK/VK/Controllers/FriendPhotosViewController.swift
+++ VK/VK/Controllers/FriendPhotosViewController.swift
@@ -8,26 +8,22 @@ class FriendPhotosViewController: UIViewController {
     // Private Constants
     enum Constants {
         static let emptyString = ""
+        static let opacity = "opacity"
     }
 
     // MARK: - Private IBoutlet
 
     @IBOutlet private var imageView: UIImageView!
 
-    // MARK: - Private Visual Components
-
-    // MARK: - Public Properties
-
     // MARK: - Private Properties
 
-    private var friendPhotoNames: [String] = []
     private lazy var firstPhoto = friendPhotoNames.first
     private lazy var lastPhoto = friendPhotoNames.last
+
+    private var friendPhotoNames: [String] = []
     private var currentPhoto = Constants.emptyString
     private var prevoiceIndex = 0
     private var currentPhotoIndex = 0
-
-    // MARK: - Initializers
 
     // MARK: - LifeCycle
 
@@ -40,25 +36,21 @@ class FriendPhotosViewController: UIViewController {
 
     func configure(_ photos: [String]) {
         friendPhotoNames = photos
-        print(photos)
     }
-
-    // MARK: - Private IBAction
 
     // MARK: - Private Methods
 
     @objc private func swipeAction(_ gesture: UISwipeGestureRecognizer) {
+        prevoiceIndex = currentPhotoIndex
         switch gesture.direction {
         case .left:
             guard currentPhoto != lastPhoto else { fallthrough }
-            prevoiceIndex = currentPhotoIndex
             currentPhotoIndex += 1
-            setCurrentPhoto(view.bounds.width)
+            animatePhotoImageView(view.bounds.width)
         case .right:
             guard currentPhoto != firstPhoto else { fallthrough }
-            prevoiceIndex = currentPhotoIndex
             currentPhotoIndex -= 1
-            setCurrentPhoto(-view.bounds.width)
+            animatePhotoImageView(-view.bounds.width)
         case .down:
             swipeDown()
         default:
@@ -67,39 +59,32 @@ class FriendPhotosViewController: UIViewController {
     }
 
     private func animatePhotoImageView(_ offSet: CGFloat) {
+        currentPhoto = friendPhotoNames[currentPhotoIndex]
         UIView.animate(
             withDuration: 0.5,
             animations: {
                 self.imageView.image = UIImage(named: self.friendPhotoNames[self.prevoiceIndex])
                 self.imageView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+                self.animateDisapearPhoto()
             },
             completion: { _ in
                 self.imageView.transform = CGAffineTransform(translationX: offSet, y: 0)
                 UIView.animate(withDuration: 1.0) {
-                    self.imageView.image = UIImage(named: self.friendPhotoNames[self.currentPhotoIndex])
+                    self.imageView.image = UIImage(
+                        named: self.friendPhotoNames[self.currentPhotoIndex]
+                    )
                     self.imageView.transform = .identity
                 }
             }
         )
     }
 
-    func animateTitle() {
-        let fadeAnimation = CABasicAnimation(keyPath: "opacity")
+    private func animateDisapearPhoto() {
+        let fadeAnimation = CABasicAnimation(keyPath: Constants.opacity)
         fadeAnimation.fromValue = 1
         fadeAnimation.toValue = 0
-        fadeAnimation.duration = 1.0
-        fadeAnimation.beginTime = CACurrentMediaTime() + 1
-        fadeAnimation.fillMode = .backwards
-
+        fadeAnimation.duration = 0.5
         imageView.layer.add(fadeAnimation, forKey: nil)
-    }
-
-    private func setCurrentPhoto(_ offSet: CGFloat) {
-        let photo = friendPhotoNames[currentPhotoIndex]
-        print(photo)
-        currentPhoto = photo
-        imageView.image = UIImage(named: currentPhoto)
-        animatePhotoImageView(offSet)
     }
 
     private func swipeDown() {
@@ -115,29 +100,21 @@ class FriendPhotosViewController: UIViewController {
         )
     }
 
-    private func configureLeftGesture() {
+    private func configureGesture(_ direction: UISwipeGestureRecognizer.Direction) {
         let gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction))
-        gesture.direction = .left
-        imageView.addGestureRecognizer(gesture)
-    }
-
-    private func configureRightGesture() {
-        let gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction))
-        gesture.direction = .right
-        imageView.addGestureRecognizer(gesture)
-    }
-
-    private func configureDownGesture() {
-        let gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction))
-        gesture.direction = .down
+        gesture.direction = direction
         imageView.addGestureRecognizer(gesture)
     }
 
     private func configureUI() {
+        configureImageView()
+        configureGesture(.left)
+        configureGesture(.right)
+        configureGesture(.down)
+    }
+
+    private func configureImageView() {
         imageView.image = UIImage(named: firstPhoto ?? Constants.emptyString)
         imageView.isUserInteractionEnabled = true
-        configureLeftGesture()
-        configureRightGesture()
-        configureDownGesture()
     }
 }

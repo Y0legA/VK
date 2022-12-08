359d3051c0838605bc11330c9daac19d57e88f6c
Oleg <myaddub@gmail.com>
2022-12-09 02:11:43 +0300

    потерянные..

---
+++ VK/VK/OperationQueue/ParseData.swift
@@ -0,0 +1,24 @@
+// ParseData.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Alamofire
+
+/// Парсинг данных
+final class ParseData: Operation {
+    // MARK: - Public Properties
+
+    var groupDetails: [GroupDetail] = []
+
+    // MARK: - Public Methods
+
+    override func main() {
+        guard let getDataOperation = dependencies.first as? GetDataOperation,
+              let data = getDataOperation.data else { return }
+        do {
+            let response = try JSONDecoder().decode(Group.self, from: data)
+            groupDetails = response.groupInfo.items
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
